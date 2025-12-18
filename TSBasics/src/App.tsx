552f232741fed708067c409523c3d79f0084e172
Mithun Urs <151752287+mithunurs17@users.noreply.github.com>
2025-12-18 09:27:28 +0000

    Add ChaiCard and Counter components

--- TSBasics/src/App.tsx
+++ TSBasics/src/App.tsx
@@ -1,5 +1,7 @@
-
 import './App.css'
+import {ChaiCard} from './components/ChaiCard.tsx'
+import { Counter } from './components/Counter.tsx'
+
 
 function App() {
   
@@ -7,8 +9,12 @@ function App() {
   return (
     <>
       <div>
-        <h1>Vite & React</h1>
-      
+        <h1>Vite + React</h1>
+        <ChaiCard name="HeadPhones" price={5000} />
+        <ChaiCard name="HeadPhones" price={99000} />
+      </div>
+      <div>
+        <Counter />
       </div>
       
     </>
---
+++ TSBasics/src/components/ChaiCard.tsx
@@ -0,0 +1,19 @@
+
+interface ChaiCardProp {
+    name: String;
+    price: number;
+    isSpecial?: boolean;
+}
+
+export function ChaiCard(
+    { name, price, isSpecial = false}: ChaiCardProp
+){
+        return (
+            <article>
+            <h2>
+            {name} {isSpecial && <span></span>}
+            </h2>
+            <p>{price}</p>
+            </article>
+        );
+    }
